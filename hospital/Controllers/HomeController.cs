63662d30bc784c9b506f20ee705706ec3b7ef6d8
hamidreza <hamidrezaxghasemi@gmail.com>
2021-12-22 18:14:08 +0330

    first commit

---
+++ hospital/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using hospital.Models;
+using System.Web.Mvc;
+using System.Globalization;
+
+namespace hospital.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        
+        hdata context = new hdata();
+       
+        // GET: Home
+        public ActionResult Index()
+        {
+            int docs = context.tbl_Doctors.Count();
+            int dep = context.tbl_Skills.Count();
+            ViewBag.docs = docs;
+            ViewBag.dep = dep;
+            return View();
+        }
+
+        public ActionResult getdep()
+        {
+            var dep = context.tbl_Skills.Select(x => new { x.pkID, x.Skill }).ToList();
+
+
+            return Json(dep, JsonRequestBehavior.AllowGet);
+
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult getdocs(int dep)
+        {
+
+            var docs = context.tbl_Doctors.Where(x => x.fkSkill == dep).Select(x => new { x.pkID, x.Name, x.Family }).ToList();
+            return Json(docs, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+        public ActionResult getvisit(int doc)
+        {
+            var visit = context.View_Visit.Where(x => x.fkDocID == doc && x.fkPID == null).Select(x => new { x.pkID, x.PDate }).ToList();
+            return Json(visit, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult setvisit(int vn, string namee, string phone, string family)
+        {
+            var c = context.tbl_Visit.Where(x => x.pkID == vn).SingleOrDefault();
+            int statee = 0;
+            if (c.fkPID == null)
+
+            {
+                int pid = 0;
+                var p = context.tbl_Patient.Where(x => x.Mobile == phone).SingleOrDefault();
+
+                if (p == null)
+                {
+                    tbl_Patient newp = new tbl_Patient();
+                    newp.Name = namee;
+                    newp.Family = family;
+                    newp.Mobile = phone;
+
+                    context.tbl_Patient.Add(newp);
+                    context.SaveChanges();
+                    var np = context.tbl_Patient.Where(x => x.Mobile == phone).SingleOrDefault();
+                    pid = np.pkID;
+                }
+                else
+                {
+                    pid = p.pkID;
+                }
+                var v = context.tbl_Visit.Where(x => x.pkID == vn).SingleOrDefault();
+                v.fkPID = pid;
+                v.fkVTID = 1;
+                v.EDate = v.SDate.AddMinutes(20);
+
+                context.SaveChanges();
+                statee = 2; //نوبت دهی انجام شد
+            }
+            else
+            {
+                statee = 1; //نوبت تمام شده
+            }
+            return Json(statee, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        public ActionResult recept()
+        {
+            ViewBag.title = "داشبرد";
+            return View();
+        }
+        public ActionResult visit_management()
+        {
+            ViewBag.title = "مدیریت نوبتها";
+            return View();
+          }
+
+        
+    }
+
+}
+
+        
+
+            
+
+
+
+
+            
---
+++ hospital/assets/js/m2.js
@@ -0,0 +1,358 @@
+﻿$(document).ready(function () {
+
+    $.post("/Home/getdep")
+
+        .done(function (res) {
+
+
+            console.log(res);
+
+            for (var item in res) {
+
+                $("#department").append(
+                    "<option>" + res[item].pkID + " - " + res[item].Skill + "</option>"
+
+
+                );
+
+            }
+
+
+
+        })
+
+        .fail(function () {
+
+
+
+        })
+
+        .always(function () {
+
+
+        });
+
+
+    $('#department').on('change', function (e) {               //get dep
+
+        var dep = $('#department').val();
+
+
+
+        var dep2 = dep.split("-");
+
+        var token = $('input[name="__RequestVerificationToken"]').val();
+
+        $.post("/Home/getdocs", { dep:                dep2[0],            __RequestVerificationToken      : token })
+
+            .done(function (res) {
+
+
+
+                $("#doctor").empty();
+
+                $("#doctor").append("<option>" + "انتخاب کنید" + "</option>");
+                for (var item in res) {
+
+                    $("#doctor").append(
+                        "<option>" + res[item].pkID + " - " + res[item].Name + " " + res[item].Family + "</option>"
+
+
+                    );
+
+                }
+
+
+
+            })
+
+            .fail(function () {
+
+
+
+            })
+
+            .always(function () {
+
+
+            });
+
+
+       
+
+
+    });
+
+
+
+    $('#doctor').on('change', function (e) {               //get dep
+
+        var doc = $('#doctor').val();
+
+
+
+        var doc2 = doc.split("-");
+
+        $.post("/Home/getvisit", { doc: doc2[0] })
+
+            .done(function (res) {
+
+                console.log(res);
+
+                $("#visit").empty();
+
+                $("#visit").append("<option>" + "انتخاب کنید" + "</option>");
+                for (var item in res) {
+
+                    $("#visit").append(
+                        "<option>" + res[item].pkID + " - " + res[item].PDate + "|" + res[item].PTime + "</option>"
+
+
+                    );
+
+                }
+
+
+
+            })
+
+            .fail(function () {
+
+
+
+            })
+
+            .always(function () {
+
+
+            });
+
+
+    });
+
+
+    
+
+
+    $("#name").focus(function () {
+
+        document.getElementById("name-v").innerHTML = "";
+
+    });
+
+   
+
+
+    $("#family").focus(function () {
+
+        document.getElementById("family-v").innerHTML = "";
+
+    });
+
+    
+
+
+    $("#phone").focus(function () {
+
+        document.getElementById("phone-v").innerHTML = "";
+
+    });
+
+   
+
+
+    $("#visit").focus(function () {
+
+        document.getElementById("visit-v").innerHTML = "";
+
+    });
+
+  
+    $(".valiid").blur(function () {
+
+        var valid = validtion(this.id);
+
+
+    });
+
+});
+
+
+function setvisit() {
+
+
+
+
+
+    var vn = $('#visit').val();
+
+    var vn2 = vn.split("-");
+
+    var namee = $('#name').val();
+
+    var phone = $('#phone').val();
+
+    var family = $('#family').val();
+
+    var valid1 = validtion("name");
+    var valid2 = validtion("family");
+    var valid3 = validtion("phone");
+    var valid4 = validtion("visit");
+
+   
+
+
+    if (valid1 && valid2 && valid3 && valid4) {
+        $.post("/Home/setvisit", { vn: vn2, namee: namee, phone: phone, family: family })
+
+            .done(function (res) {
+
+                console.log(res);
+
+
+                switch (res) {
+
+                    case 1: swal("عملیات ناموفق", "چنین نوبتی وجود ندارد", "error");
+                        break;
+                    case 2: swal("عملیات موفق", "نوبت شما با موفقیت ثبت شد", "success");
+                        break;
+
+                }
+
+
+
+
+
+
+            })
+
+            .fail(function () {
+
+                alert("خطا در برقراری ارتباط با سرور");
+
+            })
+
+            .always(function () {
+
+
+            });
+
+
+    }
+
+    else {
+
+        swal("عملیات ناموفق", "لطفا خطاهای ورودی را تصحیح کنید", "error");
+    }
+
+
+}
+
+
+function validtion(itemm) {
+
+   
+
+    var valid = true;
+
+    var namee = $('#name').val();
+
+    var family = $('#family').val();
+
+    var phone = $('#phone').val();
+
+    switch (itemm) {
+
+        case "name":
+
+            if (namee.length > 20 || namee.length <= 2) {
+
+
+                valid = false;
+
+                $("#name-v").html("نام باید بین 3 تا 20 کارکتر باشد");
+
+
+
+            }
+           
+
+                
+
+            
+
+            break;
+        case "family":
+
+
+            if (family.length > 20 || family.length <= 2) {
+
+
+                valid = false;
+
+                $("#family-v").html("نام خانوادگی باید بین 3 تا 20 کارکتر باشد");
+
+
+
+            }
+
+            break;
+
+        case "phone":
+
+
+
+            if (phone.length != 11) {
+
+
+                valid = false;
+
+                $("#phone-v").html("شماره همراه باید 11 رقم باشد");
+
+
+
+            }
+
+
+            break;
+
+        case "visit":
+
+
+            if ($("#visit").val() == "انتخاب کنید") {
+
+                valid = false;
+                $("#visit-v").html("از منو بالا یک نوبت انتخاب کنید");
+            }
+
+            break;
+
+    }
+
+   
+
+
+    
+
+    if (!valid) {
+
+        $('#' + itemm).removeClass("inputcorrect");
+        $('#' + itemm).addClass("inputerror");
+
+    }
+    else {
+
+        $('#' + itemm).removeClass("inputerror");
+        $('#' + itemm).addClass("inputcorrect");
+    }
+
+   
+
+
+  
+
+    return valid;
+
+}
+
+
